68cf934f9af11bfeaa8ff6c25f3bbb122a6af22a
jon kurinsky <jkurinsky@gmail.com>
2022-07-28 20:52:26 -0400

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import { Global } from '@emotion/react';
+import { createRoot } from 'react-dom/client';
+import App from './components/App';
+import { globalStyles } from './globalStyles';
+
+
+
+const app = document.getElementById('root')!;
+const root = createRoot(app);
+root.render(
+  <>
+    <Global styles={globalStyles} />
+    <App />
+  </>
+);
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react({ jsxImportSource: '@emotion/react' }),
+    checker({ typescript: true }),
+  ],
+  esbuild: {
+    define: {
+      // see https://github.com/evanw/esbuild/issues/2328
+      this: 'window',
+    },
+  },
+  server: {
+    open: true,
+  },
+});
